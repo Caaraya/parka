f6b52571bc8e70ed00395f532ee5caa576b2830b
caaraya <christinaaraya.ca@gmail.com>
2022-01-25 22:56:08 -0800

    frames can now be generated

--- animationgen.py
+++ animationgen.py
@@ -7,6 +7,9 @@ import sys
 def float_to_int_string(f):
     return str(int(float(f)))
 
+def float_from_pt(f):
+    return float(f.replace("pt", ''))
+
 class Animation:
     def __init__(self):
         self.argsParsed = {}
@@ -27,21 +30,22 @@ class Animation:
         composite = self.getBackgroundImage()
         for imagemeta in self.argsParsed["FRAMES"][frame]["Units"]:
             imageString = base64.b64decode(imagemeta["Image"]["Image"].partition(",")[2]).decode("utf-8")
-            ImageOrder = imagemeta["Image"]["ImageId"]
+            minutescale = imagemeta["Scale"]
             x = imagemeta["X"]
             y = imagemeta["Y"]
-            scale = 0.2 # should get from api
             txt = imageString.replace('\n', '')
             data = sg.fromstring(txt)
-            imageW = float(data.width.replace("pt", ''))
-            imageH = float(data.height.replace('pt', ''))
-            image = self.Svg(data.getroot(), [imageW,imageH ], [0,0])
-            image.scale_width_to_reference(int(float(self.argsParsed["SPRITE_WIDTH"])))
-            image.scale_by_factor(scale)
-            image.move(x, y)
-            composite.append([image.data])
-            #maybe need overall scale and individual scale, make a composite image with these
-        return composite.to_str()
+            overallWidth = float(self.argsParsed["SPRITE_WIDTH"])
+            imageW = float_from_pt(data.width)
+            imageH = float_from_pt(data.height)
+            image = data.getroot()
+            scale = overallWidth/imageW * float(minutescale)
+            newposx = float_from_pt(composite.width)/2 - x - scale*imageW/2
+            newposy = float_from_pt(composite.height)/2 - y - scale*imageH/2
+            image.moveto(newposx, newposy, scale)
+            composite.append([image])
+            #maybe need overall cale and individual scale, make a composite image with these
+        return composite.to_str().decode()
     
     def getBackgroundImage(self):
         txt = Path('background.svg').read_text()
@@ -50,32 +54,3 @@ class Animation:
         txt = txt.replace("<width>", w).replace("<height>", h)
         data = sg.fromstring(txt)
         return data
-
-    class Svg(object):
-        "svg files with a data objeSct (the svg), width, height and coordinates"
-
-        def __init__(self, data, dim, coords):
-            self.data = data
-            self.width = dim[0]
-            self.height = dim[1]
-            self.x = coords[0]
-            self.y = coords[1]
-
-        def scale_width_to_reference(self, reference_width):
-            """Proportionally scale the image to a given width."""
-            scalings_factor = reference_width / self.width
-            self.data.moveto(0, 0, scalings_factor)
-            self.width = self.width * scalings_factor
-            self.height = self.height * scalings_factor
-
-        def scale_by_factor(self, scalings_factor):
-            """Proportionally scale image by a scaling factor."""
-            self.data.moveto(0, 0, scalings_factor)
-            self.width = self.width * scalings_factor
-            self.height = self.height * scalings_factor
-
-        def move(self, x, y):
-            """Move the coordinates of an image."""
-            self.data.moveto(x, y)
-            self.x = x
-            self.y = y
--- app/animationGenerator.go
+++ app/animationGenerator.go
@@ -13,12 +13,10 @@ type Image struct {
 }
 
 type Unit struct {
-	overal_scale float64
-	x_scale      float64
-	y_scale      float64
-	X            float64
-	Y            float64
-	Image        Image
+	X     float64
+	Y     float64
+	Scale float64
+	Image Image
 }
 
 type Frame struct {
@@ -30,104 +28,64 @@ type Animation struct {
 	FrameWidth  float64
 	FrameHeight float64
 	Name        string
-	MaxImages   int64
-}
-
-func (u *Unit) clearScale() {
-	u.overal_scale = 0
-	u.x_scale = 0
-	u.y_scale = 0
-}
-
-func (u *Unit) SetScale(x float64, y float64) int {
-	if x <= 0 {
-		return 1 // unset error
-	}
-	if y >= 0 {
-		u.x_scale = x
-		u.y_scale = y
-	} else {
-		u.overal_scale = x
-	}
-	return 0
-}
-func (u *Unit) getScale() []float64 {
-	if u.overal_scale >= 0 {
-		return []float64{u.overal_scale}
-	} else if u.x_scale >= 0 && u.y_scale >= 0 {
-		return []float64{u.x_scale, u.y_scale}
-	}
-	return nil
 }
 
 func GetDefault() Animation {
-	sample_image := "<svg xmlns:\"http://www.w3.org/2000/svg\" xmlns:xlink:\"http://www.w3.org/1999/xlink\" width:\"160pt\" height:\"160pt\" viewBoX:\"0 0 160 160\" version:\"1.1\">\n<g id:\"surface1\">\n<path style:\"fill-rule:nonzero;fill:rgb{56.078431%,100%,98.823529%};fill-opacitY:1;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb{46.666667%,13.333333%,20%};stroke-opacitY:0.7;stroke-miterlimit:10;\" d:\"M 2.919043 2.559863 L 2.936523 2.570508 C 2.879785 2.66377 2.809375 2.747949 2.727637 2.82041 L 2.903223 3.018359 C 2.763086 3.142676 2.598828 3.236719 2.420703 3.294629 L 2.60498 3.861523 C 1.854883 4.105273 1.031836 3.875098 0.51709 3.277539 L 0.499121 3.292969 C 0.206445 2.953418 0.0376953 2.524512 0.0204102 2.076563 L 0.613477 2.053613 C 0.60166 1.747363 0.691602 1.445703 0.869238 1.195898 L 1.112793 1.369141 C 1.179395 1.275488 1.260254 1.193066 1.352637 1.124805 L 0.94375 0.572168 C 1.262793 0.336133 1.651563 0.21377 2.048242 0.224609 L 2.041504 0.472754 C 2.224121 0.477734 2.404492 0.51543 2.573828 0.584082 L 2.560938 0.615723 C 2.876367 0.743555 3.138672 0.975293 3.304492 1.272461 Z M 2.919043 2.559863 \" transform:\"matrix{40,0,0,40,0,0}\"/>\n</g>\n</svg>"
+	sample_image := "<svg xmlns:\"http://www.w3.org/2000/svg\" xmlns:xlink:\"http://www.w3.org/1999/xlink\" width:\"160pt\" height:\"160pt\" viewBoX:\"0 0 160 160\" version:\"1.1\">\n<g id:\"surface1\">\n<path style:\"fill-rule:nonzero;fill:rgb{206.078431%,100%,98.8232029%};fill-opacitY:1;stroke-width:0.2;stroke-linecap:butt;stroke-linejoin:miter;stroke:rgb{46.666667%,13.333333%,20%};stroke-opacitY:0.7;stroke-miterlimit:10;\" d:\"M 2.919043 2.20209863 L 2.9362023 2.20702008 C 2.8797820 2.66377 2.8093720 2.747949 2.727637 2.82041 L 2.903223 3.0183209 C 2.762086 3.142676 2.2098828 3.236719 2.420703 3.294629 L 2.60498 3.8612023 C 1.8204883 4.1020273 1.031836 3.8720098 0.201709 3.2772039 L 0.499121 3.292969 C 0.2064420 2.9203418 0.03769203 2.20242012 0.0204102 2.0762063 L 0.613477 2.0203613 C 0.60166 1.747363 0.691602 1.4420703 0.869238 1.1920898 L 1.112793 1.369141 C 1.1793920 1.2720488 1.2602204 1.192066 1.3202637 1.1248020 L 0.943720 0.2072168 C 1.262793 0.336133 1.62012063 0.21377 2.048242 0.224609 L 2.0412004 0.4727204 C 2.224121 0.477734 2.404492 0.2012043 2.2073828 0.2084082 L 2.2060938 0.6120723 C 2.876367 0.743202020 3.138672 0.9720293 3.204492 1.272461 Z M 2.919043 2.20209863 \" transform:\"matrix{40,0,0,40,0,0}\"/>\n</g>\n</svg>"
 
 	one := Image{sample_image, 1}
 	two := Image{sample_image, 2}
 	three := Image{sample_image, 3}
 	four := Image{sample_image, 4}
 	five := Image{sample_image, 5}
+	six := Image{sample_image, 6}
 
 	walk_forward := Animation{
 		Name:        "walk-forward",
-		FrameWidth:  10,
-		FrameHeight: 10,
-		MaxImages:   5,
+		FrameWidth:  100,
+		FrameHeight: 100,
 		Frames: []Frame{
 			{
 				Units: []Unit{
-					{X: -5, Y: -5, Image: one},
-					{X: 5, Y: 5, Image: two},
-					{X: 0, Y: 0, Image: three},
-					{X: -5, Y: 5, Image: four},
-					{X: 5, Y: 5, Image: five},
+					{X: -20, Y: -20, Image: one, Scale: 0.4},
+					{X: 20, Y: 20, Image: two, Scale: 0.4},
+					{X: 0, Y: 0, Image: three, Scale: 0.6},
+					{X: -20, Y: 20, Image: four, Scale: 0.4},
+					{X: 20, Y: -20, Image: five, Scale: 0.4},
+					{X: 0, Y: 25, Image: six, Scale: 0.5},
 				},
 			},
 			{
 				Units: []Unit{
-					{X: 5, Y: 5, Image: two},
-					{X: -5, Y: 5, Image: four},
-					{X: 0, Y: 0, Image: three},
-					{X: 5, Y: -5, Image: five},
-					{X: -5, Y: -5, Image: one},
+					{X: 20, Y: 20, Image: two, Scale: 0.4},
+					{X: -20, Y: 20, Image: four, Scale: 0.4},
+					{X: 0, Y: 0, Image: three, Scale: 0.6},
+					{X: 20, Y: -20, Image: five, Scale: 0.4},
+					{X: -20, Y: -20, Image: one, Scale: 0.4},
+					{X: 0, Y: 25, Image: six, Scale: 0.5},
 				},
 			},
 			{
 				Units: []Unit{
-					{X: 5, Y: -5, Image: five},
-					{X: -5, Y: 5, Image: four},
-					{X: 0, Y: 0, Image: three},
-					{X: 5, Y: 5, Image: two},
-					{X: -5, Y: -5, Image: one},
+					{X: 20, Y: -20, Image: five, Scale: 0.4},
+					{X: -20, Y: 20, Image: four, Scale: 0.4},
+					{X: 0, Y: 0, Image: three, Scale: 0.6},
+					{X: 20, Y: 20, Image: two, Scale: 0.4},
+					{X: -20, Y: -20, Image: one, Scale: 0.4},
+					{X: 0, Y: 25, Image: six, Scale: 0.5},
 				},
 			},
 			{
 				Units: []Unit{
-					{X: 5, Y: 5, Image: two},
-					{X: -5, Y: 5, Image: four},
-					{X: 0, Y: 0, Image: three},
-					{X: 5, Y: -5, Image: five},
-					{X: -5, Y: -5, Image: one},
+					{X: 20, Y: 20, Image: two, Scale: 0.4},
+					{X: -20, Y: 20, Image: four, Scale: 0.4},
+					{X: 0, Y: 0, Image: three, Scale: 0.6},
+					{X: 20, Y: -20, Image: five, Scale: 0.4},
+					{X: -20, Y: -20, Image: one, Scale: 0.4},
+					{X: 0, Y: 25, Image: six, Scale: 0.5},
 				},
 			},
 		},
-	}
-
-	for i, v := range walk_forward.Frames {
-		if i%2 == 0 {
-			v.Units[0].SetScale(0.2, 0)
-			v.Units[1].SetScale(0.2, 0)
-			v.Units[2].SetScale(0.5, 0)
-			v.Units[3].SetScale(0.1, 0)
-			v.Units[4].SetScale(0.1, 0)
-		} else {
-			v.Units[0].SetScale(0.15, 0)
-			v.Units[1].SetScale(0.15, 0)
-			v.Units[2].SetScale(0.5, 0)
-			v.Units[3].SetScale(0.15, 0)
-			v.Units[4].SetScale(0.15, 0)
-		}
 	}
 
 	return walk_forward
